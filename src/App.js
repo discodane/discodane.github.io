afacf2b5a69ce3f711911676a5533e4a2a899a57
Dane Erickson <jisade2@MacBook141.local>
2019-02-11 08:19:53 -0600

    got stuff there

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route, 
+  Switch, 
+} from 'react-router-dom';
+import Header from './Header';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Switch>
+            <Route exact path="/"  />
+            <Route path="/about"  />
+            <Route path="/courses"  />
+            <Route exact path="/teachers"  />
+            <Route path="/teachers/:topic/:teachersName" />
+            <Route />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
