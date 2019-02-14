bc7811dfcff1a7cb3f5724e90443c1a38046307a
Dane Erickson <jisade2@MacBook141.local>
2019-02-14 08:55:17 -0600

    messing with styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,10 +3,12 @@ import './Home.css';
 
 const Home = () => (
   <div className="content">
-    <div className="name"><span>NAME</span></div>
-    <h3>Developer</h3>
-    <div>
-      Brief welcome
+    <div className='text'>
+      <div className="name"><span>NAME</span></div>
+      <h3>Developer</h3>
+      <div>
+        welcome
+      </div>
     </div>
   </div>
 )
